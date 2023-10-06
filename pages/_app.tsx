152a108e4697c797c5712c80462cf361a82f63c0
khkang <alkalisummer@gmail.com>
2023-10-06 19:53:33 +0900

    hashtag css 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,6 +46,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <meta property='og:url' content='https://keylog.hopto.org'></meta>
           <meta property='og:image' content='https://keylog.hopto.org/keyword.png'></meta>
           <meta property='og:description' content='인기 키워드를 활용한 블로그 포스팅'></meta>
+          <meta name='viewport' content='initial-scale=1.0, user-scalable=no, maximum-scale=1, width=device-width' />
         </Head>
         <Component {...pageProps} />
       </div>
--- pages/components/Hashtag.tsx
+++ pages/components/Hashtag.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Hashtag = ({ hashtag, setHashtag, hashtagArr, setHashtagArr }: { hashtag: string; setHashtag: React.Dispatch<React.SetStateAction<string>>; hashtagArr: string[]; setHashtagArr: React.Dispatch<React.SetStateAction<string[]>> }) => {
   //이벤트 실행순서 : onKeyDown -> onKeyPress -> onChange -> onKeyUp 이므로 onChange로 먼저 상태변경 후 onKeyUp에서 comma와 enter를 구분자로 split
@@ -14,9 +14,9 @@ const Hashtag = ({ hashtag, setHashtag, hashtagArr, setHashtagArr }: { hashtag: 
   };
 
   return (
-    <span className='post_hashtag_input_div'>
-      #<input type='text' id='post_hashtag_input' className='post_hashtag_input' value={hashtag} onChange={(e) => setHashtag(e.target.value)} onKeyUp={(e) => splitTag(e)} placeholder='태그를 입력하세요'></input>
-    </span>
+    <label htmlFor='post_hashtag_input_div' className='post_hashtag_input_div'>
+      <input id='post_hashtag_input' className='post_hashtag_input' value={hashtag} onChange={(e) => setHashtag(e.target.value)} onKeyUp={(e) => splitTag(e)} placeholder='#태그를 입력하세요'></input>
+    </label>
   );
 };
 
