6cd0de30877e55bf7f3aae681488027fcb9d61e8
khkang <alkalisummer@gmail.com>
2025-09-09 23:25:42 +0900

    fix iphone image upload test

--- src/features/post/api/uploadPostImage.ts
+++ src/features/post/api/uploadPostImage.ts
@@ -4,10 +4,11 @@ import { formatDate } from '@/shared/lib/util';
 import { objectStorageClient } from '@/shared/lib/oci';
 
 export const uploadPostImage = async (image: File) => {
+  console.log('image', image);
   const objectStorage = await objectStorageClient();
   const imageName = `IMG${formatDate({ date: new Date(), isFullTime: true })}`;
   const customImageFile = new File([image], imageName, { type: image.type });
-
+  console.log('customImageFile', customImageFile);
   try {
     await objectStorage.put(customImageFile);
     const imageUrl = `${process.env.CLOUD_BUCKET_URL}/${imageName}`;
