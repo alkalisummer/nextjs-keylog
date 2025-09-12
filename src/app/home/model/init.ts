fc865c7a9ae336d2e45f0cabcb2a2ddf59163df5
khkang <alkalisummer@gmail.com>
2025-09-12 21:13:51 +0900

    fix trending type

--- src/app/home/model/init.ts
+++ src/app/home/model/init.ts
@@ -3,8 +3,8 @@ import { Trend } from '@/entities/trend/model';
 export const initTrendData = (): Trend => ({
   keyword: '',
   articleKeys: [],
-  traffic: '',
-  trafficGrowthRate: '',
-  activeTime: '',
+  traffic: 0,
+  trafficGrowthRate: 0,
+  activeTime: new Date(),
   relatedKeywords: [],
 });
